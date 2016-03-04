29a66f949c99a35d96ec9d901bb7441f00417dec
oxa <gladhuie@gmail.com>
2016-03-04 10:08:11 +0100

    Parse each worksheet as switch configuration

--- parser.py
+++ parser.py
@@ -2,10 +2,7 @@ import jinja2
 from openpyxl import Workbook,load_workbook
 
 wb = Workbook()
-wb = load_workbook('input/L2.xlsx')
-ws=wb['Phiscal layout']
-interfaces=[]
-
+wb = load_workbook('input/L2 (1).xlsx')
 #Set columns information parameters
 #Colomun id interface name
 c_ifname=1
@@ -16,30 +13,31 @@ c_desc=4
 #Colomun id vlan id
 c_vlanid=3
 
+for ws in wb :
+    interfaces=[]
+    for row in ws.rows:
+        if (row[c_vlanid].value==u"trunk"):
+            interfaces.append({
+            "ifname" : row[c_ifname].value,
+            "vlanid":row[c_vlanid].value,
+            "ifnumber":row[c_ifnumber].value,
+            "description" :row[c_desc].value
+                })
+        elif(isinstance(row[c_vlanid].value,float)):
+            interfaces.append({
+            "ifname" : row[c_ifname].value,
+            "vlanid":int(row[c_vlanid].value),
+            "ifnumber":row[c_ifnumber].value,
+            "description" :row[c_desc].value
+        })
 
-for row in ws.rows:
-    if (row[c_vlanid].value==u"trunk"):
-        interfaces.append({
-        "ifname" : row[c_ifname].value,
-        "vlanid":row[c_vlanid].value,
-        "ifnumber":row[c_ifnumber].value,
-        "description" :row[c_desc].value
-            })
-    elif(isinstance(row[c_vlanid].value,float)):
-        interfaces.append({
-        "ifname" : row[c_ifname].value,
-        "vlanid":int(row[c_vlanid].value),
-        "ifnumber":row[c_ifnumber].value,
-        "description" :row[c_desc].value
-    })
 
+    templateLoader = jinja2.FileSystemLoader( searchpath="templates" )
+    templateEnv = jinja2.Environment( loader=templateLoader )
+    TEMPLATE_FILE = "switch-interfaces"
+    template = templateEnv.get_template( TEMPLATE_FILE )
+    templateVars={"interfaces":interfaces}
 
-templateLoader = jinja2.FileSystemLoader( searchpath="templates" )
-templateEnv = jinja2.Environment( loader=templateLoader )
-TEMPLATE_FILE = "switch-interfaces"
-template = templateEnv.get_template( TEMPLATE_FILE )
-templateVars={"interfaces":interfaces}
-
-outputText = template.render( templateVars )
-with open("conf/device.cfg", "wb") as fh:
-    fh.write(outputText)
+    outputText = template.render( templateVars )
+    with open("conf/"+ws.title+".cfg", "wb") as fh:
+        fh.write(outputText)
