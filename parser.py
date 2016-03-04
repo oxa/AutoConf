5ae13422b412014184cdede20353a2de8c5ca66a
Oxa <gladhuie@gmail.com>
2016-03-05 00:18:47 +0100

    Add l3 configuration support

--- parser.py
+++ parser.py
@@ -1,9 +1,19 @@
 import jinja2
 from openpyxl import Workbook,load_workbook
 
-wb = Workbook()
+def cast(val, to_type, default=None):
+    try:
+        return to_type(val)
+    except (ValueError,TypeError):
+        default=val
+        return default
+
+l2wb = Workbook()
+l3wb = Workbook()
 #Load the excel configuration file
-wb = load_workbook('input/L2 (1).xlsx')
+l2wb = load_workbook('input/L2 (1).xlsx')
+l3wb = load_workbook('input/L3.xlsx')
+
 #Set columns information parameters
 #Colomun id interface name
 c_ifname=1
@@ -14,31 +24,46 @@ c_desc=4
 #Colomun id vlan id
 c_vlanid=3
 
-for ws in wb :
-    interfaces=[]
-    for row in ws.rows:
-        if (row[c_vlanid].value==u"trunk"):
-            interfaces.append({
-            "ifname" : row[c_ifname].value,
-            "vlanid":row[c_vlanid].value,
-            "ifnumber":row[c_ifnumber].value,
-            "description" :row[c_desc].value
-                })
-        elif(isinstance(row[c_vlanid].value,float)):
-            interfaces.append({
-            "ifname" : row[c_ifname].value,
-            "vlanid":int(row[c_vlanid].value),
-            "ifnumber":row[c_ifnumber].value,
-            "description" :row[c_desc].value
-        })
+# for l2ws in l2wb :
+#     l2interfaces=[]
+#     for row in l2ws.rows:
+#         if (row[c_vlanid].value==u"trunk"):
+#             l2interfaces.append({
+#             "ifname" : row[c_ifname].value,
+#             "vlanid":row[c_vlanid].value,
+#             "ifnumber":row[c_ifnumber].value,
+#             "description" :row[c_desc].value
+#                 })
+#         elif(isinstance(row[c_vlanid].value,float)):
+#             l2interfaces.append({
+#             "ifname" : row[c_ifname].value,
+#             "vlanid":int(row[c_vlanid].value),
+#             "ifnumber":row[c_ifnumber].value,
+#             "description" :row[c_desc].value
+#         })
+
+for l3ws in l3wb :
+    l3interfaces=[]
+    for row in l3ws.rows:
+        l3interfaces.append({
+        "ifname" : row[0].value,
+        "vlanid":cast(row[2].value,int),
+        "ifnumber":cast(row[1].value,int),
+        "description" :row[7].value,
+        "ipaddr" : row[5].value,
+        "netmask" : row[6].value,
+        "subint" : cast(row[2].value,int),
+        "autoconf": row[3].value
+            })
 
 
     templateLoader = jinja2.FileSystemLoader( searchpath="templates" )
     templateEnv = jinja2.Environment( loader=templateLoader )
-    TEMPLATE_FILE = "switch-interfaces"
+    TEMPLATE_FILE = "l3-cfg"
     template = templateEnv.get_template( TEMPLATE_FILE )
-    templateVars={"interfaces":interfaces}
+   # templateVars={"l2interfaces":l2interfaces,"l3interfaces":l3interfaces}
+    templateVars={"l3interfaces":l3interfaces}
 
-    outputText = template.render( templateVars )
-    with open("conf/"+ws.title+".cfg", "wb") as fh:
+    outputText = template.render( templateVars,trim_blocks=True,lstrip_blocks=True )
+    with open("conf/"+l3ws.title+".cfg", "wb") as fh:
         fh.write(outputText)
